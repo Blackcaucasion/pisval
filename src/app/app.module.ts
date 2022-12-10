55010a4e8e5667c112b7ad5019a74ad30d4b59b9
luntu <lteencwadi@gmail.com>
2022-12-10 09:50:36 +0200

    first commit
    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbAccordionModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CustomAccordianComponent } from './custom-accordian/custom-accordian/custom-accordian.component';
+import { UiAccordianComponent } from './ui-accordian/ui-accordian.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomAccordianComponent,
+    UiAccordianComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbAccordionModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
